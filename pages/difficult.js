e81a44535b5f346efbdf81d96ce1afd85189f78c
RafDes21 <israeltineo84@gmail.com>
2022-04-03 19:28:52 -0300

    nuevo

---
+++ pages/difficult.js
@@ -0,0 +1,33 @@
+const number1 = document.getElementById(`number1`)
+num1 = number1.innerHTML = numeroAleatorio()
+
+const number2 = document.getElementById(`number2`)
+num2 = number2.innerHTML = numeroAleatorio()
+
+function numeroAleatorio() {
+    return Math.round(Math.random() * 10000)
+}
+
+function result() {
+    return num1 + num2
+}
+
+function probar() {
+    return document.getElementById(`probar`).value
+
+}
+
+const btn = document.getElementById(`btn`)
+btn.addEventListener(`click`, () => {
+
+    if (result() === parseInt(probar())) {
+        const final=document.getElementById(`result-final`)
+        final.innerHTML=`FELICITACIONES!!! ES CORRECTO`
+        const resultado = document.getElementById(`result`)
+        resultado.innerHTML = probar()
+    } else {
+        const resultado = document.getElementById(`result`)
+        resultado.innerHTML =  `NO ES LA RESPUESTA, PERO PODES VOLVER A INTENTARLO`
+    }
+
+})
